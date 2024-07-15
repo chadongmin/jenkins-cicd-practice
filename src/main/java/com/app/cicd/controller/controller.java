d0f5bd42a1a6d25b2451d2ff196601929b5e89a4
chadongmin <cdm2883@naver.com>
2024-07-15 22:03:28 +0900

    controller

--- src/main/java/com/app/cicd/controller/controller.java
+++ src/main/java/com/app/cicd/controller/controller.java
@@ -1,2 +1,14 @@
-package com.app.cicd.controller;public class controller {
+package com.app.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@GetMapping("")
+	public String home() {
+		return "Hello World";
+	}
+
 }
